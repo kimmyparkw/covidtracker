649d44f586f4c90adf7503507c0609832efa6b2e
Shannon Maloy <shannon.maloy@gmail.com>
2020-08-28 16:50:40 -0400

    Server.js Setup.  Added Proxy to React package.json

---
+++ server.js
@@ -0,0 +1,59 @@
+//import our dependencies
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
+
+//intialize the app and setup dotenv
+const app = express()
+require('dotenv').config()
+
+//Middleware Setup
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+    session({
+        key: process.env.SECRET_KEY,
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: true,
+    })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.static('public'))
+
+//Set the port to listen
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+//ROUTES SETUP
+
+//Root Route
+app.get('/', (req, res) => {
+    res.send('Hello World!  Welcome To Our Covid-19 Tracker')
+})
+
+
+
+
+//Error handlers
+app.use('*', (req, res) => {
+    res.status(400).json({
+        message: 'Not Found!!'
+    })
+})
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        error: err,
+        message: err.message
+    })
+}) 

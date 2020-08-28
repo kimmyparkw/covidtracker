e034a84080a99149dc9f93757e1288b06fdcc79d
Shannon Maloy <shannon.maloy@gmail.com>
2020-08-28 19:00:15 -0400

    Set up User model, auth-helper.js, Local.js and Passport.js

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const db = require('../db/config')
+
+class User {
+    constructor({ id, username, email, password_digest }) {
+        this.id = id || null;
+        this.username = username;
+        this.email = email;
+        this.password_digest = password_digest;
+    }
+
+    static findByUserName(username) {
+        return db.oneOrNone(`
+        SELECT * FROM users WHERE username = $1`, username)
+            .then(user => {
+                if (user) return new this(user)
+                throw new Error(`No user with username: ${username} found.`)
+        })
+    }
+
+    //SEARCH BY USERID - Will be usered for User-States
+    static getById(id) {
+        return db.oneOrNone("SELECT * FROM users WHERE id = $1", id)
+          .then((user) => {
+            if (user) return new this(user);
+            throw new Error("No user found.");
+          });
+      }
+
+    //CREATE NEW USER
+    save() {
+        return db.one(`INSERT INTO users
+        (username, email, password_digest)
+        VALUES ($/username/, $/email/, $/password_digest/) RETURNING *`, this)
+        .then(user => Object.assign(this, user))
+    }
+
+    //UPDATE USER PROFILE
+    update(changes) {
+        Object.assign(this, changes)
+        return db.oneOrNone(
+            `UPDATE users SET
+            email = $/email/, password_digest = $/password_digest/ RETURNING *`, this)
+            .then(user => {
+            return Object.assign(this, user)
+        })
+    }
+}
+
+module.exports = User

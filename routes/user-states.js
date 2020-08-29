adbbbf04b62adf63c198193578484f857b8862eb
Megan Lee <megselayne@gmail.com>
2020-08-29 11:35:38 -0400

    push up for shannon

--- routes/user-states.js
+++ routes/user-states.js
@@ -4,8 +4,6 @@ const userStatesRouter = express.Router();
 const userStatesController = require('../controllers/users-states-controller');
 // const authHelpers = require('../services/auth-helpers');
 
-userStatesRouter.get('/profile', userStatesController.index);
-
 userStatesRouter.post('/:id([a-z][a-z])', userStatesController.create);
 
 userStatesRouter.delete('/:id([0-9]+)', userStatesController.destroy);

1a28de8f2554761812f7a1455f32f8203a6aaff9
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-08-30 19:53:03 -0500

    initial setup of header/navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,17 @@
 //includes the logo and the nav
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
-        <h1>nav will go here</h1>
+        <div className="header">
+            <h1>Logo will go here</h1>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/stats'>Stats</Link>
+                <Link to='/auth/login'>Login</Link>
+            </nav>
+        </div>
     )
 }

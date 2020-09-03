2633bed92676e491073d4b876c9f4bd328ee1e92
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-09-03 10:31:42 -0500

    adding divs to profile page

--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -12,15 +12,18 @@ class Profile extends React.Component {
             <>
             <h1 className='welcome'>{`Welcome Back, ${this.props.user.username}!`}</h1>
             <h3 className='edit-user-link'><a className='edit-user-link' href={`/user/profile/${this.props.user.id}`}>Edit User Profile</a></h3>
+            <div className="saved-states-container">
             {
                     this.props.userSelected.map((stateData, index) => (
-                    <>
-                    <Link to={`/stats/${stateData[index].state}`}><h2>{this.props.fullName[stateData[index].state]}</h2></Link>
-                    <HistoricalChart key={stateData[index].hash} delete={this.props.delete} fullName={this.props.fullName} stateName={stateData[index].state} chartData={stateData} />
-                    <button onClick={() => this.props.delete(this.props.stateName)}>Delete from profile</button>
-                    </>
+                    <div className="saved-state">
+                        <Link to={`/stats/${stateData[index].state}`}><h2>{this.props.fullName[stateData[index].state]}</h2></Link>
+                        <HistoricalChart key={stateData[index].hash} delete={this.props.delete} fullName={this.props.fullName} stateName={stateData[index].state} chartData={stateData} />
+                        <button onClick={() => this.props.delete(this.props.stateName)}>Delete from profile</button>
+                    </div>
                 ))
             }
+            </div>
+
             </>
         )
     }

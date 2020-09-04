8196a0ea82e0fae44934dc40330de36ba2411519
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-09-04 12:06:28 -0500

    start hamburger nav

--- client/src/components/stateless/Header.jsx
+++ client/src/components/stateless/Header.jsx
@@ -4,27 +4,27 @@ import Logo from '../../covidlogo.png'
 
 export default function Header(props) {
     return (
-        <div className="nav-container">
+        <nav className="nav-container">
             <img src={Logo} alt='logo'/>
+            <div className="burger-toggle">
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </div>
+            <ul className='navlinks'>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/stats'>Stats</Link>
+                </li>
+                    {props.auth && <li><Link to='/user/profile'>Profile</Link></li>}
+                     {props.auth ? (<li><Link className='log' to='/auth/logout' onClick={props.logout}>Logout</Link></li>) : (<li><Link className='log' to='/auth/login'>Login</Link></li>)}
+            </ul>
 
-                <ul className='navlinks'>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li>
-                        <Link to='/stats'>Stats</Link>
-                    </li>
-                        {props.auth && <li><Link to='/user/profile'>Profile</Link></li>}
-                        {props.auth ? (<li><Link className='log' to='/auth/logout' onClick={props.logout}>Logout</Link></li>) : (<li><Link className='log' to='/auth/login'>Login</Link></li>)}
-                </ul>
-                {/* <div className="burger">
-                    <div className="line1"></div>
-                    <div className="line2"></div>
-                    <div className="line3"></div>
-                </div> */}
-        </div>
+        </nav>
     )
 }

9202cdd63335bfde59cdd00b044290993cf5bfad
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-09-03 20:23:13 -0500

    starting hamburger nave

--- client/src/components/stateless/Header.jsx
+++ client/src/components/stateless/Header.jsx
@@ -8,11 +8,24 @@ export default function Header(props) {
         <div className="nav-container">
             <img src={Logo} alt='logo'/>
             <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/stats'>Stats</Link>
-                {props.auth && <Link to='/user/profile'>Profile</Link>}
-                {props.auth ? (<Link to='/auth/logout' onClick={props.logout}>Logout</Link>) : (<Link to='/auth/login'>Login</Link>)}
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/stats'>Stats</Link>
+                    </li>
+                        {props.auth && <li><Link to='/user/profile'>Profile</Link></li>}
+                        {props.auth ? (<li><Link className='log' to='/auth/logout' onClick={props.logout}>Logout</Link></li>) : (<li><Link className='log' to='/auth/login'>Login</Link></li>)}
+                </ul>
+                <div className="burger">
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
             </nav>
         </div>
     )

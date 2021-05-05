5a868702f08226534bac4700ef5eaf2865a6e9be
Shannon Maloy <shannon.maloy@gmail.com>
2021-05-05 11:57:57 -0400

    Added code to handle null/missing data from fetch

--- client/src/components/data/StateSingle.jsx
+++ client/src/components/data/StateSingle.jsx
@@ -12,7 +12,7 @@ class StateSingle extends React.Component {
                     <div className='stats'>
                         <h4>Total positive cases: {this.props.usData.positive.toLocaleString()}</h4>
                         <h4>Total negative cases: {this.props.usData.negative.toLocaleString()}</h4>
-                        <h4>Total recovered: {this.props.usData.recovered.toLocaleString()}</h4>
+                        <h4>Total recovered: {this.props.usData.recovered ? this.props.usData.recovered.toLocaleString() : "Not Availalbe"}</h4>
                         <h4>Total tests: {this.props.usData.totalTestResults.toLocaleString()}</h4>
                     </div>
                 </div> 
@@ -52,8 +52,8 @@ class StateSingle extends React.Component {
                     <div className='stats-container-right'>
                         <div className='total-stats'>
                             <h3>Total positive cases: {this.props.singleStateData.positive.toLocaleString()}</h3>
-                            <h3>Total negative cases: {this.props.singleStateData.negative.toLocaleString()}</h3>
-                            <h3>Total recovered: {this.props.singleStateData.recovered ? this.props.singleStateData.recovered.toLocaleString() : ("Not Available")}</h3>
+                            <h3>Total negative cases: {this.props.singleStateData.negative ? this.props.singleStateData.negative.toLocaleString() : "Not Available"}</h3>
+                            <h3>Total recovered: {this.props.singleStateData.recovered ? this.props.singleStateData.recovered.toLocaleString() : "Not Available"}</h3>
                             <h3>Total tests: {this.props.singleStateData.totalTestResults.toLocaleString()}</h3>
                         </div>    
                     </div>
